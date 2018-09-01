18fd00859846ec5eb1300bbd796195980e083d9a
yelena guliyeva <gulbadam@gulbadam.com>
2018-08-31 17:23:04 -0700

    initial

---
+++ App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import firebase from 'firebase';
+import {Header} from './src/components/common';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  componentWillMount = () => {
+    firebase.initializeApp({
+      apiKey: "AIzaSyB6Ax8PfbvujZMXgpIIhq0zEuDCubwFJkY",
+      authDomain: "authentication-e6107.firebaseapp.com",
+      databaseURL: "https://authentication-e6107.firebaseio.com",
+      projectId: "authentication-e6107",
+      storageBucket: "authentication-e6107.appspot.com",
+      messagingSenderId: "943815583173"
+    });
+  };
+  
+  render() {
+    return (
+      <View >
+       <Header headerText="Authentication"/>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+       
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+class LoginForm extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
+
+export default LoginForm;
